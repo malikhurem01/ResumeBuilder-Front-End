9ab70fea3a76781f89e682a5b9a7d7dd24f912fa
Senahid Ramic <senahidramic0204@gmail.com>
2023-06-11 03:04:36 +0200

    Slight modification

--- src/Utils/routes.js
+++ src/Utils/routes.js
@@ -2,5 +2,7 @@ const URL = 'https://localhost:7045';
 
 module.exports = {
   login: `${URL}/api/Auth/login`,
-  register: `${URL}/api/Auth/register`
+  register: `${URL}/api/Auth/register`,
+  currentUser: `${URL}/api/Auth/current/user`,
+  userInfo: `${URL}/api/Information/all/info`
 };
--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Login from './Components/Login';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Register from './Components/Register';
-import PDFEditor from './Components/PDFEditor';
-import Landing from './Components/Landing';
+import { BrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Landing />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Register />
-  },
-  {
-    path: '/editor',
-    element: <PDFEditor />
+import { AuthContextProvider } from './Store/auth-context-api';
+import services from './Services/authService';
+
+const initialize = async () => {
+  let token = JSON.parse(localStorage.getItem('user_jwt'));
+  let response,
+    user = null;
+  if (token) {
+    try {
+      response = await services.currentUser(token);
+      user = response.data.data;
+    } catch (err) {
+      localStorage.removeItem('user_jwt');
+    }
+  } else {
+    user = null;
   }
-]);
+  console.log(user);
+  return user;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+
+const startApplication = user => {
+  root.render(
+    <AuthContextProvider userData={user}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
+  );
+};
+
+initialize().then(startApplication);
